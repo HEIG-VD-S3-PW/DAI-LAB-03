289eb8d68aefb4a605da27664da31b5cd4f9d31f
mathieuemery <mathieu.emery@heig-vd.ch>
2024-11-11 10:50:51 +0100

    Ajout du socket TCP pour le serveur

---
+++ src/main/java/ch/heigvd/dai/TCPServer.java
@@ -0,0 +1,18 @@
+package ch.heigvd.dai;
+
+// https://medium.com/@gaurangjotwani/creating-a-tcp-connection-between-two-servers-in-java-27fabe53deaa
+
+import java.net.*;
+import java.io.IOException;
+
+public class TCPServer {
+    public static void main(String[] args) throws IOException {
+        // Here, we create a Socket instance named socket
+        ServerSocket serverSocket = new ServerSocket(5001);
+        System.out.println("Listening for clients...");
+        Socket clientSocket = serverSocket.accept();
+        String clientSocketIP = clientSocket.getInetAddress().toString();
+        int clientSocketPort = clientSocket.getPort();
+        System.out.println("[IP: " + clientSocketIP + " ,Port: " + clientSocketPort +"]  " + "Client Connection Successful!");
+    }
+}
