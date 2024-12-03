b0e3cffc4097b13ba7ee2ea50e404c3b6e48cc2f
mathieuemery <mathieu.emery@heig-vd.ch>
2024-12-03 09:16:10 +0100

    Edit the readme with the new help page

--- src/main/java/ch/heigvd/dai/commands/Client.java
+++ src/main/java/ch/heigvd/dai/commands/Client.java
@@ -11,9 +11,10 @@ import picocli.CommandLine;
         description = {
                 "Start the client to connect to the streaming server.",
                 "\nOnce connected the following commands are available:",
-                "DELETE     Delete a video that you select with it's number.",
-                "LIST       Show all videos from the server",
-                "WATCH      Watch a specific video by using it's number",
+                "DELETE <id>   Delete a video that you select with it's number.",
+                "LIST          Show all videos from the server",
+                "WATCH <id>    Watch a specific video by using it's number",
+                "UPLOAD        Upload a new video",
         },
         footer = "\nCredits: Tristan Baud, Arno Tribolet and Mathieu Emery",
         headerHeading = "\n=== Client Command ===\n\n",    // Adds a header for better separation
