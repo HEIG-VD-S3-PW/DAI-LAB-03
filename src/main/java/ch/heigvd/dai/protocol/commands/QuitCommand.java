2b0003816807ababb52cf529d64b851f3d7e0778
Arno Tribolet <arno.tribolet@hes-so.ch>
2024-12-05 13:57:18 +0100

    Add execute() for QuitCommand and suppressUser() for StreamingVideo

--- src/main/java/ch/heigvd/dai/protocol/commands/QuitCommand.java
+++ src/main/java/ch/heigvd/dai/protocol/commands/QuitCommand.java
@@ -4,6 +4,7 @@ import ch.heigvd.dai.User;
 import ch.heigvd.dai.protocol.Command;
 import ch.heigvd.dai.protocol.CommandException;
 import ch.heigvd.dai.protocol.CommandResponse;
+import ch.heigvd.dai.protocol.CommandResponseCode;
 import ch.heigvd.dai.server.StreamingVideo;
 
 public class QuitCommand extends Command {
@@ -16,7 +17,13 @@ public class QuitCommand extends Command {
 
     @Override
     public CommandResponse execute(User user, StreamingVideo streamingVideo, String[] args) {
-        return null;
+        try {
+            if (!streamingVideo.suppressUser(user)) return new CommandResponse(CommandResponseCode.NOT_FOUND,
+                                                                    "User not found");
+        } catch (Exception e) {
+            return new CommandResponse(CommandResponseCode.ERROR, e.getMessage());
+        }
+        return new CommandResponse(CommandResponseCode.OK, "See you soon :)");
     }
 
     @Override
@@ -24,7 +31,8 @@ public class QuitCommand extends Command {
         try {
             CommandResponse response = readResponse();
             if(response.getCode() != 200){
-               System.err.println("Error while quitting ASP: " + response.getMessage());
+               System.err.println("Error while quitting ASP: " + response.getMessage() +
+                                "\nYou won't be able to reconnect with the same username and email");
                return;
             }
             System.out.println(response.getMessage());
