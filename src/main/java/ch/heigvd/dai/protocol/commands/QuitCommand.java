5380305dbf963bcb67ed4539cc3c0222751b2a48
Arno Tribolet <arno.tribolet@hes-so.ch>
2024-12-05 11:46:29 +0100

    Adding template for quit command

---
+++ src/main/java/ch/heigvd/dai/protocol/commands/QuitCommand.java
@@ -0,0 +1,26 @@
+package ch.heigvd.dai.protocol.commands;
+
+import ch.heigvd.dai.User;
+import ch.heigvd.dai.protocol.Command;
+import ch.heigvd.dai.protocol.CommandException;
+import ch.heigvd.dai.protocol.CommandResponse;
+import ch.heigvd.dai.server.StreamingVideo;
+
+public class QuitCommand extends Command {
+    public QuitCommand() { super("QUIT", "Close connection with the server"); }
+
+    @Override
+    public void validate(String[] args) throws CommandException {
+
+    }
+
+    @Override
+    public CommandResponse execute(User user, StreamingVideo streamingVideo, String[] args) {
+        return null;
+    }
+
+    @Override
+    public void receive() {
+
+    }
+}
